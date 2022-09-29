888e66159bbd838f44db6c4d2afd0ef22ad0b8d8
OBITO <90102005+Vibe-OBITO@users.noreply.github.com>
2022-09-30 01:56:28 +0300

    Delete Лабораторная №2 directory

--- Лабораторная №2/1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(void) {
-    printf("Hello  KaLLi\n");
-    return 0;
-}
--- Лабораторная №2/script1.sh
+++
@@ -1,7 +0,0 @@
-#!/home/noname/021731_Аблакулов_Амаль
-echo "$@" > file
-for i in "$@"
-     do
-       echo "$i"
-    done
-exit 0
