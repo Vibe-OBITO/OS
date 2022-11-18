9154089b16e05ae9ae55cf7349f2fe14900f0382
OBITO <90102005+Vibe-OBITO@users.noreply.github.com>
2022-11-18 19:16:02 +0300

    Add files via upload

---
+++ Лабораторная №5/CMakeFiles/FileSystem.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/noname/021731_Аблакулов_Амаль/лаб5/command.cpp" "CMakeFiles/FileSystem.dir/command.cpp.o" "gcc" "CMakeFiles/FileSystem.dir/command.cpp.o.d"
+  "/home/noname/021731_Аблакулов_Амаль/лаб5/commonfunc.cpp" "CMakeFiles/FileSystem.dir/commonfunc.cpp.o" "gcc" "CMakeFiles/FileSystem.dir/commonfunc.cpp.o.d"
+  "/home/noname/021731_Аблакулов_Амаль/лаб5/filecommand.cpp" "CMakeFiles/FileSystem.dir/filecommand.cpp.o" "gcc" "CMakeFiles/FileSystem.dir/filecommand.cpp.o.d"
+  "/home/noname/021731_Аблакулов_Амаль/лаб5/init.cpp" "CMakeFiles/FileSystem.dir/init.cpp.o" "gcc" "CMakeFiles/FileSystem.dir/init.cpp.o.d"
+  "/home/noname/021731_Аблакулов_Амаль/лаб5/load.cpp" "CMakeFiles/FileSystem.dir/load.cpp.o" "gcc" "CMakeFiles/FileSystem.dir/load.cpp.o.d"
+  "/home/noname/021731_Аблакулов_Амаль/лаб5/main.cpp" "CMakeFiles/FileSystem.dir/main.cpp.o" "gcc" "CMakeFiles/FileSystem.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Лабораторная №5/CMakeFiles/FileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileSystem.dir/command.cpp.o"
+  "CMakeFiles/FileSystem.dir/command.cpp.o.d"
+  "CMakeFiles/FileSystem.dir/commonfunc.cpp.o"
+  "CMakeFiles/FileSystem.dir/commonfunc.cpp.o.d"
+  "CMakeFiles/FileSystem.dir/filecommand.cpp.o"
+  "CMakeFiles/FileSystem.dir/filecommand.cpp.o.d"
+  "CMakeFiles/FileSystem.dir/init.cpp.o"
+  "CMakeFiles/FileSystem.dir/init.cpp.o.d"
+  "CMakeFiles/FileSystem.dir/load.cpp.o"
+  "CMakeFiles/FileSystem.dir/load.cpp.o.d"
+  "CMakeFiles/FileSystem.dir/main.cpp.o"
+  "CMakeFiles/FileSystem.dir/main.cpp.o.d"
+  "FileSystem"
+  "FileSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
